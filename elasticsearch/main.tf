1f5134f86b130497423ee24aa5e1c35bf00045b9
Your Name <you@example.com>
2019-10-07 03:32:38 +0000

    wip

--- elasticsearch/main.tf
+++ elasticsearch/main.tf
@@ -5,7 +5,7 @@ resource "null_resource" "set_database_values" {
 
   provisioner "local-exec" {
     command = <<EOF
-ruby scripts/local/set_elasticsearch.rb DATABASE_HOST=${var.database.address} DATABASE_NAME=${var.database.name} DATABASE_USERNAME=${var.database.username} DATABASE_PASSWORD=${var.database.password} DATABASE_ENVIRONMENT=${terraform.workspace} S3_NAME=${var.s3.tags.Name} S3_BUCKET=${var.s3.bucket} S3_ENVIRONMENT=${terraform.workspace} ECR_URL=${var.ecr.repository_url} ECR_ENVIRONMENT=${terraform.workspace}
+ruby scripts/local/set_elasticsearch.rb DATABASE_HOST=${var.database.address} DATABASE_NAME=${var.database.name} DATABASE_USERNAME=${var.database.username} DATABASE_PASSWORD=${var.database.password} DATABASE_ENVIRONMENT=production S3_NAME=${var.s3.tags.Name} S3_BUCKET=${var.s3.bucket} S3_ENVIRONMENT=production ECR_URL=${var.ecr.repository_url} ECR_NAME=${var.ecr.name} ECR_ENVIRONMENT=production
 EOF
   }
 }
